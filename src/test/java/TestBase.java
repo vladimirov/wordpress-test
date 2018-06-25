47c76100a6f2bacb04fd9ef7cc1961e27902a49b
vladimirov <denis.vladimiroff@gmail.com>
2018-06-25 19:46:27 +0300

    testng.xml added

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -3,10 +3,7 @@ import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.ITestContext;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -19,20 +16,20 @@ public class TestBase {
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
-    @BeforeSuite
+    @BeforeTest
     public void setUp(ITestContext context) throws Exception {
         app.init();
         context.setAttribute("app", app);
     }
 
-    @AfterSuite(alwaysRun = true)
+    @AfterTest(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
 
     @BeforeMethod
     public void logTestStart(Method m, Object[] p) {
-        logger.info("START TEST " + m.getName() + " WITH PARAMETERS " + Arrays.asList(p));
+        logger.info("START TEST " + m.getName());
     }
 
     @AfterMethod(alwaysRun = true)
