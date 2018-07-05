89b8d47e3db276761cd8903be6827641a7f91084
vladimirov <denis.vladimiroff@gmail.com>
2018-07-06 01:53:55 +0300

    pagination test added

--- src/main/java/pages/FaviconPage.java
+++ src/main/java/pages/FaviconPage.java
@@ -4,7 +4,12 @@ import appmanager.HelperBase;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FaviconPage extends HelperBase {
 
@@ -12,14 +17,21 @@ public class FaviconPage extends HelperBase {
         super(driver);
     }
 
-    public boolean faviconIsOnSite() {
-
-        String link = "https://github.com";
-
-        Document doc = Jsoup.parse(link);
-        Element element = doc.head().select("link[href~=.*\\.(ico|png)]").first();
-        String href = getElementAttribute("href");
-        return element.attr("href").equals(href);
+    public List<Elements> elementsWithFavicons() throws IOException {
+        String url = "https://wp-dev.space/ism/kitchen/develop/";
+//        String url = "https://wp-dev.space/ism/countersteer/master/";
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.head().select("link[href~=.*\\.(ico|png)]");
+        List<Elements> list = new ArrayList<>();
+        for (Element link : links) {
+            list.add(link.getElementsByAttribute("href"));
+//            System.out.println(link.getElementsByAttribute("href"));
+        }
+        return list;
     }
-
 }
+//                <link rel="icon" href="https://wp-dev.space/ceatus/ybsg/develop/" +
+//                "wp-content/uploads/2018/05/cropped-fav-icon-32x32.png" sizes="32x32">
+
+//        Document document = Jsoup.parse(url);
+
--- src/test/java/ui/FaviconTest.java
+++ src/test/java/ui/FaviconTest.java
@@ -1,7 +1,11 @@
 package ui;
 
 import appmanager.TestBase;
+import org.jsoup.select.Elements;
 import org.testng.annotations.Test;
+
+import javax.lang.model.element.Element;
+import java.io.IOException;
 
 import static org.testng.Assert.assertTrue;
 
@@ -9,10 +13,16 @@ import static org.testng.Assert.assertTrue;
 public class FaviconTest extends TestBase {
 
     @Test
-    public void testFavIcon1() {
-//        app.openBaseUrl();
+    public void testFavIcon1() throws IOException {
         app.openTest();
-        assertTrue(app.faviconPage().faviconIsOnSite());
+        System.out.println(app.faviconPage().elementsWithFavicons().size());
+
+        for (Elements e : app.faviconPage().elementsWithFavicons())  {
+            System.out.println(e);
+        }
+
+        
+
     }
 
 }
