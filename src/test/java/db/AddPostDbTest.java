db017f31b0ec2bb03c5b18bbef354766689843cb
vladimirov <denis.vladimiroff@gmail.com>
2018-12-11 23:12:05 +0200

    add post in database

--- src/test/java/db/AddPostDbTest.java
+++ src/test/java/db/AddPostDbTest.java
@@ -12,27 +12,29 @@ public class AddPostDbTest extends TestBase {
 
     @Test
     public void testPostCreation() {
-//        Posts before = app.db().posts();
-//        System.out.println("Number of posts is: " + before.size());
-//        PostData post = new PostData().withId(Integer.MAX_VALUE).withTitle("Test DB Creation");
-
         try {
             // create a mysql database connection
             String myDriver = "com.mysql.cj.jdbc.Driver";
             String myUrl = "jdbc:mysql://192.168.1.5/wordpress.local";
             Class.forName(myDriver);
             Connection conn = DriverManager.getConnection(myUrl, "root", "");
+            // create a sql date object so we can use it in our INSERT statement
+            Calendar calendar = Calendar.getInstance();
+            java.sql.Date date = new java.sql.Date(calendar.getTime().getTime());
             // the mysql insert statement
-            String query = "insert into wp_posts (post_title, post_content, post_excerpt, to_ping, pinged, post_content_filtered) " +
-                    "values (?, ?, ?, ?, ?, ?)";
+            String query = "insert into wp_posts " +
+                    "(post_title, post_date, post_content, post_excerpt, to_ping, pinged, post_content_filtered, post_name) " +
+                    "values (?, ?, ?, ?, ?, ?, ?, ?)";
             // create the mysql insert prepared statement
             PreparedStatement preparedStmt = conn.prepareStatement(query);
             preparedStmt.setString(1, "Test Post JDBC");
-            preparedStmt.setString(2, "Test Post JDBC");
-            preparedStmt.setString(3, "");
+            preparedStmt.setDate(2, date);
+            preparedStmt.setString(3, app.admin().testContent());
             preparedStmt.setString(4, "");
             preparedStmt.setString(5, "");
             preparedStmt.setString(6, "");
+            preparedStmt.setString(7, "");
+            preparedStmt.setString(8, "test-post");
             // execute the prepared statement
             preparedStmt.execute();
             conn.close();
