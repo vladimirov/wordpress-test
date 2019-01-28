1eb12475258aa7afdfef8aa0d9a725ac63664b3d
vladimirov <denis.vladimiroff@gmail.com>
2019-01-28 19:28:05 +0200

    cmd line parser added

--- src/main/java/appmanager/Appender.java
+++ src/main/java/appmanager/Appender.java
@@ -3,6 +3,9 @@ package appmanager;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
 import org.gitlab4j.api.models.Issue;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -13,26 +16,41 @@ import java.util.Properties;
 
 public class Appender {
 
-    public static int projectId;
+    @Option(name = "-id", usage = "Sets a project id")
+    public static String projectId;
     public static String path = "src/main/resources/local.properties";
 
     public static void main(String[] args) throws IOException, GitLabApiException {
+        new Appender().doAppender(args);
+    }
+
+    private void doAppender(String[] args) throws IOException, GitLabApiException {
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+            this.run();
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
+        }
+    }
+
+    public void run() throws IOException, GitLabApiException, CmdLineException {
         Properties gitlabProperties = new Properties();
         gitlabProperties.load(new FileReader(new File("src/main/resources/gitlab.properties")));
-
-        System.out.print("\n\rEnter project ID: ");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        projectId = Integer.parseInt(reader.readLine());
-
+        int id = Integer.parseInt(projectId);
         GitLabApi gitLabApi = new GitLabApi(gitlabProperties.getProperty("gitlabHostUrl"), gitlabProperties.getProperty("gitlabApiToken"));
-        Issue credentials = gitLabApi.getIssuesApi().getIssue(projectId, 1);
+        Issue credentials = gitLabApi.getIssuesApi().getIssue(id, 1);
         new PrintWriter(path).print("");
-
         String creds = credentials.getDescription();
         String webBaseUrl = creds.split("\\[")[2].split("]")[0];
         String webAdminLogin = creds.split("user: ")[1].split("<")[0];
         String webAdminPass = creds.split("password: ")[1].split("<")[0];
-        String input = "projectId = " + projectId + "\r\n" + "web.baseUrl = " + webBaseUrl + "/" + "\r\n" + "web.adminLogin = " + webAdminLogin + "\r\n" + "web.adminPassword = " + webAdminPass;
+        String input =
+                "projectId = " + id + "\r\n" +
+                "web.baseUrl = " + webBaseUrl + "/" + "\r\n" +
+                "web.adminLogin = " + webAdminLogin + "\r\n" +
+                "web.adminPassword = " + webAdminPass;
         Files.write(
                 Paths.get(path),
                 input.getBytes(),
@@ -40,7 +58,6 @@ public class Appender {
         System.out.println("-------------------------------------------------------");
         System.out.println("SITE CREDENTIALS ARE SUCCESSFULLY ADDED");
         System.out.println("-------------------------------------------------------");
-
-
     }
+
 }
