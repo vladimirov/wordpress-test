63a4b84d9243991b2b50f9e5ab80b927eb6020e4
vladimirov <denis.vladimiroff@gmail.com>
2019-02-27 12:33:14 +0200

    add test post via api added

---
+++ src/test/java/ui/AddPostTest.java
@@ -0,0 +1,41 @@
+package ui;
+
+import appmanager.TestBase;
+
+import com.afrozaar.wordpress.wpapi.v2.Wordpress;
+import com.afrozaar.wordpress.wpapi.v2.config.ClientConfig;
+import com.afrozaar.wordpress.wpapi.v2.config.ClientFactory;
+import com.afrozaar.wordpress.wpapi.v2.exception.PostCreateException;
+import com.afrozaar.wordpress.wpapi.v2.model.Post;
+import com.afrozaar.wordpress.wpapi.v2.model.PostStatus;
+import com.afrozaar.wordpress.wpapi.v2.model.builder.ContentBuilder;
+import com.afrozaar.wordpress.wpapi.v2.model.builder.ExcerptBuilder;
+import com.afrozaar.wordpress.wpapi.v2.model.builder.PostBuilder;
+import com.afrozaar.wordpress.wpapi.v2.model.builder.TitleBuilder;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static appmanager.ApplicationManager.baseUrl;
+import static appmanager.ApplicationManager.adminLogin;
+import static appmanager.ApplicationManager.adminPassword;
+
+public class AddPostTest extends TestBase {
+
+    @Test()
+    public void addTestPostViaApi() throws IOException, PostCreateException {
+        final Wordpress client = ClientFactory.fromConfig(ClientConfig.of(baseUrl, adminLogin, adminPassword, false, false));
+
+        final Post post = PostBuilder.aPost()
+                .withTitle(TitleBuilder.aTitle().withRendered("Title_from_Wordpress_Java_API").build())
+                .withExcerpt(ExcerptBuilder.anExcerpt().withRendered("").build())
+                .withContent(ContentBuilder.aContent().withRendered(app.admin().testContent()).build())
+                .build();
+
+        final Post createdPost = client.createPost(post, PostStatus.publish);
+
+        System.out.println(createdPost.getLink());
+
+    }
+
+}
