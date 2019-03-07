601f0e4c81c07b6c0281138d5d6a46179f7d10b1
vladimirov <denis.vladimiroff@gmail.com>
2019-03-07 19:31:30 +0200

    report properties changed

--- src/main/java/appmanager/ApplicationManager.java
+++ src/main/java/appmanager/ApplicationManager.java
@@ -277,7 +277,8 @@ public class ApplicationManager {
         SlackSession session = SlackSessionFactory.createWebSocketSlackSession(slackApiBotToken);
         session.connect();
         SlackChannel channel = session.findChannelByName("simple-tests"); //make sure bot is a member of the channel.
-        session.sendFile(channel, Files.readAllBytes(Paths.get("report-" + Reporter.id + ".pdf")), "Report");
+//        session.sendFile(channel, Files.readAllBytes(Paths.get("report-" + Reporter.id + ".pdf")), "Report");
+        session.sendFile(channel, Files.readAllBytes(Paths.get("/home/denis/autotests/wordpress-test/report-" + Reporter.id + ".pdf")), "Report");
         session.disconnect();
     }
 
